9e53efb40683bf479946094d411f569ed45387ce
Koch Viktor <viktor.koch@oxit.hu>
2024-05-12 21:01:53 +0200

    Create empty VisualStudio .NET MAUI 7 project

---
+++ Budgeting/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Budgeting
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
